c1e2f6504b31d3c62c1bb6d014442730db09cd8c
cua <dul311298@gmail.com>
2024-11-03 11:12:04 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BROKEN LINK HIJACKING BY FELCITY</h1>
+    </div>
+  );
+}
+
+export default App;
